9ff53531907b58e8cba444a8aa925fca55a8397f
amir <amyrmdi99@gmail.com>
2026-07-03 21:45:17 +0330

    feat(app): implement main application loop

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,32 @@ import util.Menu;
 public class Main {
     public static void main(String[] args) {
 
+        boolean running = true;
+
         Menu menu = new Menu();
 
+        while (running){
         int choice = menu.getUserChoice();
 
-        System.out.println("You selected: " + choice);
+        switch (choice){
+
+            case 1:
+                System.out.println("Add Task Selected");
+                break;
+
+            case 2:
+                System.out.println("Show Task selected");
+                break;
+
+            case 3:
+                System.out.println("Good Bye!");
+                running = false;
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+        }
+        }
 
     }
 }
